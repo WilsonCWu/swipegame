088f81d97fe15ca865d14e0cc38bacf5cbbe4a20
Wilson Wu <wilsoncwu@Wilsons-MacBook-Pro.local>
2024-08-04 00:05:35 -0400

    stats

--- swipegame/Assets/Scripts/GameController.cs
+++ swipegame/Assets/Scripts/GameController.cs
@@ -110,26 +110,59 @@ class Card
 
     public override string ToString()
     {
-        string rankString = "";
-        switch (Rank)
+        return SuitToString(Suit) + RankToString(Rank);
+    }
+
+    public static string SuitToString(Suit suit)
+    {
+        switch(suit)
+        {
+            case Suit.Spades:
+                return "♠";
+            case Suit.Hearts:
+                return "♥";
+            case Suit.Diamonds:
+                return "♦";
+            case Suit.Clubs:
+                return "♣";
+            default:
+                return "";
+        }
+    }
+
+    public static string RankToString(Rank rank)
+    {
+        switch(rank)
         {
             case Rank.Ace:
-            rankString = "A";
-            break;
-            case Rank.King:
-            rankString = "K";
-            break;
-            case Rank.Queen:
-            rankString = "Q";
-            break;
+                return "A";
+            case Rank.Two:
+                return "2";
+            case Rank.Three:
+                return "3";
+            case Rank.Four:
+                return "4";
+            case Rank.Five:
+                return "5";
+            case Rank.Six:
+                return "6";
+            case Rank.Seven:
+                return "7";
+            case Rank.Eight:
+                return "8";
+            case Rank.Nine:
+                return "9";
+            case Rank.Ten:
+                return "10";
             case Rank.Jack:
-            rankString = "J";
-            break;
+                return "J";
+            case Rank.Queen:
+                return "Q";
+            case Rank.King:
+                return "K";
             default:
-            rankString = ((int)Rank).ToString();
-            break;
+                return "";
         }
-        return rankString + Suit.ToString().Substring(0, 1);
     }
 }
 
@@ -163,6 +196,8 @@ public class GameController : MonoBehaviour
     public TextMeshProUGUI curCardText;
     public TextMeshProUGUI[] selectedCardTexts;
     public TextMeshProUGUI cardsLeftText;
+    public TextMeshProUGUI drawPileContentsText;
+    public TextMeshProUGUI countsLeftText;
     private GameState gameState;
 
 
@@ -178,6 +213,49 @@ public class GameController : MonoBehaviour
             selectedCardTexts[i].text = "";
         }
         cardsLeftText.text = "Cards Left: " + gameState.DrawPile.Count;
+        // update draw pile contents in sorted order. 
+        drawPileContentsText.text = "";
+        List<Card> sortedDrawPile = new List<Card>(gameState.DrawPile);
+        sortedDrawPile.Sort((a, b) =>
+        {
+            int rankComparison = a.Rank.CompareTo(b.Rank);
+            if (rankComparison == 0)
+            {
+            return a.Suit.CompareTo(b.Suit);
+            }
+            return rankComparison;
+        });
+        foreach (Card card in sortedDrawPile)
+        {
+            drawPileContentsText.text += card.ToString() + ", ";
+        }
+        // also show the number left of each card number, and each suit
+        countsLeftText.text = "";
+        foreach (Rank rank in System.Enum.GetValues(typeof(Rank)))
+        {
+            int count = 0;
+            foreach (Card card in gameState.DrawPile)
+            {
+                if (card.Rank == rank)
+                {
+                    count++;
+                }
+            }
+            countsLeftText.text += Card.RankToString(rank) + ": " + count + "\n";
+        }
+        foreach (Suit suit in System.Enum.GetValues(typeof(Suit)))
+        {
+            int count = 0;
+            foreach (Card card in gameState.DrawPile)
+            {
+                if (card.Suit == suit)
+                {
+                    count++;
+                }
+            }
+            countsLeftText.text += Card.SuitToString(suit) + ": " + count + "\n";
+        }
+        
     }
     // Start is called before the first frame update
     void Start()
